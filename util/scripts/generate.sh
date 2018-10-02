0bc5494454da6a5b3fa9707f57af27a2dc6b60cb
Jacky Romano <jacky.romano@intel.com>
2018-10-02 15:57:07 +0300

    [generate.sh] refer to default python executable

--- util/scripts/generate.sh
+++ util/scripts/generate.sh
@@ -1,11 +1,13 @@
 echo
 echo generating...
 
-cd ../../
-mkdir -p build
-cd build
+MYDIR=$(dirname $0)
+ROOT=$MYDIR/../..
+
+mkdir -p $ROOT/build
+cd $ROOT/build
 
 # you can specify a custom install location and the python version
-cmake -DCMAKE_INSTALL_PREFIX=~/open3d_install .. #-DPYTHON_EXECUTABLE=/usr/bin/python3.5
+cmake -DCMAKE_INSTALL_PREFIX=~/open3d_install .. -DPYTHON_EXECUTABLE=$(which python) #-DPYTHON_EXECUTABLE=/usr/bin/python3.5
 
 echo
